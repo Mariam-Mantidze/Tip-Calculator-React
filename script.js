6599ef6aa2235a37d9079a90b76bd96dfb7a5319
Mariam <mariam.mantidze1@gmail.com>
2024-02-05 18:04:20 +0400

    wip functiocal, rewriting

--- script.js
+++ script.js
@@ -1,66 +1,107 @@
-const billInput = document.getElementById("bill");
-const tipInputs = Array.from(document.getElementsByClassName("tip"));
-const peopleInput = document.getElementById("people");
-const resultTip = document.getElementById("result-tip");
-const resultTotal = document.getElementById("result-total");
-const resetButtton = document.getElementById("reset");
-const customTipInput = document.getElementById("custom-tip");
+document.addEventListener("DOMContentLoaded", function () {
+  const billInput = document.getElementById("bill");
+  const tipInputs = document.querySelectorAll(".tip");
+  const peopleInput = document.getElementById("people");
+  const resultTip = document.getElementById("result-tip");
+  const resultTotal = document.getElementById("result-total");
+  const resetButtton = document.getElementById("reset");
+  const customTipInput = document.getElementById("custom-tip");
 
-let billValue = 0;
-let tipValue = 0;
-let peopleValue = 0;
+  let billValue = 0;
+  let tipValue = 0;
+  let peopleValue = 0;
 
-billInput.addEventListener("input", (event) => {
-  billValue = Number(event.target.value);
-  calculation();
-});
-
-tipInputs.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    tipValue = parseInt(event.target.innerText);
+  billInput.addEventListener("input", (event) => {
+    billValue = Number(event.target.value);
     calculation();
   });
-});
 
-peopleInput.addEventListener("input", (event) => {
-  peopleValue = Number(event.target.value);
-  calculation();
-});
+  tipInputs.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      tipValue = parseInt(event.target.innerText);
+      calculation();
+    });
+  });
 
-customTipInput.addEventListener("input", (event) => {
-  tipValue = Number(event.target.value);
-  calculation();
-});
+  peopleInput.addEventListener("input", (event) => {
+    peopleValue = Number(event.target.value);
+    calculation();
+  });
 
-function calculation() {
-  if (peopleValue != 0) {
-    // define calculation value
+  customTipInput.addEventListener("input", (event) => {
+    tipValue = Number(event.target.value);
+    calculation();
+  });
 
-    // tip value per person
-    let tipResultValue = (billValue * (tipValue / 100)) / peopleValue;
-    // total value per person
-    let totalValue = billValue / peopleValue + tipResultValue;
+  function calculation() {
+    if (peopleValue != 0) {
+      // tip per person
+      let tipResultValue = (billValue * (tipValue / 100)) / peopleValue;
 
-    // update tip UI with result
-    resultTip.innerText = `$${tipResultValue.toFixed(2)}`;
-    // update total UI with result
-    resultTotal.innerText = `$${totalValue.toFixed(2)}`;
-  } else {
-    // handle empty input / update UI back to 0.
-    resultTip.innerText = "$0.00";
-    resultTotal.innerText = "$0.00";
+      // total per person
+      let totalValue = billValue / peopleValue + tipResultValue;
+
+      // update UI with results
+      resultTip.innerText = `$${tipResultValue.toFixed(2)}`;
+      resultTotal.innerText = `$${totalValue.toFixed(2)}`;
+    }
   }
-}
-
-resetButtton.addEventListener("click", () => {
-  billValue = 0;
-  tipValue = 0;
-  peopleValue = 0;
-  resultTip.innerText = "$0.00";
-  resultTotal.innerText = "$0.00";
-  billInput.value = "";
-  peopleInput.value = "";
-  customTipInput.value = "";
 });
+
+// function calculation() {
+//   if (peopleValue != 0) {
+//     // define calculation value
+
+//     // tip value per person
+//     let tipResultValue = (billValue * (tipValue / 100)) / peopleValue;
+//     // total value per person
+//     let totalValue = billValue / peopleValue + tipResultValue;
+
+//     // update tip UI with result
+//     resultTip.innerText = `$${tipResultValue.toFixed(2)}`;
+//     // update total UI with result
+//     resultTotal.innerText = `$${totalValue.toFixed(2)}`;
+//   } else {
+//     // handle empty input / update UI back to 0.
+//     resultTip.innerText = "$0.00";
+//     resultTotal.innerText = "$0.00";
+//   }
+// }
+
+// resetButtton.addEventListener("click", () => {
+//   billValue = 0;
+//   tipValue = 0;
+//   peopleValue = 0;
+//   resultTip.innerText = "$0.00";
+//   resultTotal.innerText = "$0.00";
+//   billInput.value = "";
+//   peopleInput.value = "";
+//   customTipInput.value = "";
+// });
 
 // ქასთომოსთვის რესულტ ველიუ არ შევქნათ"
+
+// let tipValue = 0;
+// let peopleValue = 0;
+
+// billInput.addEventListener("input", (event) => {
+//   billValue = Number(event.target.value);
+//   calculation();
+// });
+
+// tipInputs.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     tipValue = parseInt(event.target.innerText);
+//     calculation();
+//   });
+// });
+
+// peopleInput.addEventListener("input", (event) => {
+//   peopleValue = Number(event.target.value);
+//   calculation();
+// });
+
+// customTipInput.addEventListener("input", (event) => {
+//   tipValue = Number(event.target.value);
+//   calculation();
+// });
